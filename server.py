5215b33a2161033f394e67e24d7e53d65df182be
gstrenge <gstrenge01@gmail.com>
2025-12-05 21:56:04 -0500

    Work in progress queue system

--- server.py
+++ server.py
@@ -8,7 +8,10 @@ import logging
 from pathlib import Path
 import simpleaudio as sa
 from collections import OrderedDict
+from queue import Queue
 from typing import List
+import datetime
+from dataclasses import dataclass
 from time import time
 
 logging.basicConfig(
@@ -25,6 +28,23 @@ SHOW = [
     {"t": 8, "id": "79563461152192-14", "state": 1.0},
     # ...
 ]
+
+@dataclass
+class DailySchedule:
+    start_time: datetime.time
+    end_time: datetime.time
+
+@dataclass
+class LightCue:
+    t: float
+    id: str
+    state: float
+
+@dataclass
+class LightShow:
+    song_name: str
+    music_file: Path
+    light_cues: List[LightCue]
 
 WHITE_CHRISTMAS = [
     # Intialize start to on
@@ -85,8 +105,8 @@ WHITE_CHRISTMAS = [
     {"t": 0.0, "id": "2B", "state": 0.0},
     {"t": 0.0, "id": "3A", "state": 0.0},
     {"t": 0.0, "id": "3B", "state": 0.0},
-    {"t": 0.0, "id": "4A", "state": 0.0},
-    {"t": 0.0, "id": "4B", "state": 0.0},
+    {"t": 0.0, "id": "4A", "state": 1.0},
+    {"t": 0.0, "id": "4B", "state": 1.0},
     {"t": 0.0, "id": "6A", "state": 0.0},
     {"t": 0.0, "id": "6B", "state": 0.0},
 
@@ -97,10 +117,10 @@ WHITE_CHRISTMAS = [
     {"t": 2.6, "id": "6A", "state": 1.0},
 
     {"t": 3.75, "id": "2B", "state": 1.0},
-    {"t": 3.75, "id": "4A", "state": 1.0},
+    # {"t": 3.75, "id": "4A", "state": 1.0},
 
     {"t": 5.12, "id": "2A", "state": 1.0},
-    {"t": 5.12, "id": "4B", "state": 1.0},
+    # {"t": 5.12, "id": "4B", "state": 1.0},
 
     {"t": 6.21, "id": "3B", "state": 1.0},
     {"t": 5.21, "id": "3A", "state": 1.0},
@@ -113,13 +133,20 @@ WHITE_CHRISTMAS = [
     {"t": 8.78, "id": "6B", "state": 0.0},
 
     {"t": 10.05, "id": "2B", "state": 0.0},
-    {"t": 10.05, "id": "4A", "state": 0.0},
+    # {"t": 10.05, "id": "4A", "state": 0.0},
 
     {"t": 10.54, "id": "2A", "state": 0.0},
-    {"t": 10.54, "id": "4B", "state": 0.0},
+    # {"t": 10.54, "id": "4B", "state": 0.0},
 
     {"t": 11.20, "id": "3B", "state": 0.0},
     {"t": 11.20, "id": "3A", "state": 0.0},
+    
+    {"t": 11.20, "id": "SNOWMACHINE_1", "state": 1.0},
+    {"t": 11.20, "id": "4A", "state": 0.0},
+    {"t": 11.20, "id": "4B", "state": 0.0},
+    {"t": 28.20, "id": "SNOWMACHINE_1", "state": 0.0},
+    
+
 ]
 
 SHOW = [
@@ -128,13 +155,32 @@ SHOW = [
 
 class WebsocketServer:
 
-    def __init__(self, host, port, id_mapping: Dict[str, str]):
+    def __init__(self, host, port, id_mapping: Dict[str, str], playlist: List[LightShow], schedule: DailySchedule):
         self.host = host
         self.port = port
         self.id_mapping = id_mapping
         self.reverse_id_mapping = {value: key for key, value in id_mapping.items()}
+        
+        # Represents the schedule of when to start playing and when to stop playing
+        self.schedule = schedule
+        
+        # Represents the playlist being repeated
+        self.playlist: List[LightShow] = playlist
+        # Represents the playlist queue
+        self.queue: Queue[LightShow] = Queue()
+        # Represents the actively requested songs that have priority
+        self.request_queue: Queue[LightCue] = Queue()
 
         self.connections: Dict[str, Any] = {}
+        
+    def skip(self):
+        pass
+    
+    def pause(self):
+        pass
+    
+    def play(self):
+        pass
 
     def parse_connect_message(self, message: str) -> List[str]:
         data = json.loads(message)
@@ -198,6 +244,34 @@ class WebsocketServer:
             if cmd_type == "c":
                 logging.info("Connections:")
                 logging.info("\n".join(self.connections.keys()))
+                
+            elif cmd_type == "q":
+                logging.info("Adding to queue")
+                self.request_queue.put(LightShow("test", Path("test.mp3"), []))
+                
+            elif cmd_type == "snow":
+                logging.info("Turning on snow machine")
+                snowmachine_id = self.reverse_id_mapping.get("SNOWMACHINE_1")
+                spotlight1_id = self.reverse_id_mapping.get("4A")
+                spotlight2_id = self.reverse_id_mapping.get("4B")
+                
+                await asyncio.gather(
+                    self.send_state(snowmachine_id, 1.0),
+                    self.send_state(spotlight1_id, 0.0),
+                    self.send_state(spotlight2_id, 0.0),
+                )
+                
+            elif cmd_type == "nosnow":
+                logging.info("Turning off snow machine")
+                snowmachine_id = self.reverse_id_mapping.get("SNOWMACHINE_1")
+                spotlight1_id = self.reverse_id_mapping.get("4A")
+                spotlight2_id = self.reverse_id_mapping.get("4B")
+                
+                await asyncio.gather(
+                    self.send_state(snowmachine_id, 0.0),
+                    self.send_state(spotlight1_id, 1.0),
+                    self.send_state(spotlight2_id, 1.0),
+                )
 
             # Set
             elif cmd_type == "sr":
@@ -229,10 +303,49 @@ class WebsocketServer:
             logging.info(f"Sent to {id}: {state}")
         except Exception as e:
             logging.warning(f"Error sending to {id}: {e}")
+            
+    async def run(self):
+        try:
+            logging.info("Beginning queue")
+            while True:
+                now = datetime.datetime.now().time()
+                # If we are out of the scheduled time
+                # if now < self.schedule.start_time or now > self.schedule.end_time:
+                #     logging.info("Outside of schedule")
+                #     await asyncio.sleep(60)
+                #     continue
+                
+                # We are inside the current scheduled time
+                logging.info("Inside of schedule, playing next")
 
-    async def play_show(self, wav_path: Path, cues: List):
+                # Check queue first
+                if not self.request_queue.empty():
+                    logging.info("Playing from request queue")
+                    song = self.request_queue.get()
+                else:
+                    logging.info("Playing from playlist queue")
+                    if self.queue.empty():
+                        logging.info("Refilling queue")
+                        # If the queue is empty, refill it
+                        for show in self.playlist:
+                            self.queue.put(show)
+                    song = self.queue.get()
+                await self.play_show(song)
+        except Exception as e:
+            logging.error(e)
+            logging.error(e.with_traceback())
+    def get_queue(self):
+        """Gets the current view only queue that reflects the order of which songs are played next"""
+        return list(self.request_queue.queue) + list(self.queue.queue)
+    
+    async def play_show(self, show: LightShow):
+        logging.info(f"Playing {show.song_name}")
+        await asyncio.sleep(10)
+        logging.info(f"Finished {show.song_name}")
 
-        await asyncio.sleep(8)
+    async def play_show_old(self, wav_path: Path, cues: List):
+
+        await asyncio.sleep(10)
 
         logging.info("Loading from wav")
         audio_file = sa.WaveObject.from_wave_file(str(wav_path))
@@ -290,12 +403,25 @@ async def main():
     id_mapping_path = Path(__file__).parent / "id_mapping.json"
     with open(id_mapping_path, "r") as id_mapping_file:
         id_mapping = json.load(id_mapping_file)
+        
+    white_christmas1 = LightShow("White Christmas 1", "./short.wav", [])
+    white_christmas2 = LightShow("White Christmas 2", "./short.wav", [])
+    white_christmas3 = LightShow("White Christmas 3", "./short.wav", [])
+    white_christmas_req = LightShow("White Christmas Requested", "./short.wav", [])
+    
+    playlist = [white_christmas1, white_christmas2, white_christmas3]
+    
+    # Make a schedule with datetime that just represents time of day with no dates
+    
+    schedule = DailySchedule(datetime.time(17, 0), datetime.time(23, 0))
 
     host = "192.168.1.10"
     port = 8765
-    server = WebsocketServer(host, port, id_mapping)
+    server = WebsocketServer(host, port, id_mapping, playlist, schedule)
     
-    await asyncio.gather(server.serve(), server.console(), server.play_show(Path("./whitechristmas.wav"), WHITE_CHRISTMAS))
+    await asyncio.gather(server.serve(), server.console(), server.play_show_old(Path("./whitechristmas.wav"), WHITE_CHRISTMAS))
+    # await asyncio.gather(server.console(), server.run())
+    # await asyncio.gather(server.serve(), server.console(), server.run())
     # await asyncio.gather(server.serve(), server.console())
 
 if __name__ == "__main__":
